72005fee468e52221c3215bbcce70a820d39fcd8
raketa0 <rychkovalex04@gmail.com>
2025-10-15 01:30:47 +0300

    правки

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -7,9 +7,9 @@ CRITICAL_SECTION cs;
 
 DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
 {
-	int threadNum = (int)(intptr_t)lpParam;
+	int threadNum = *(int*) lpParam;
 	EnterCriticalSection(&cs);
-	std::cout << "Поток № " << threadNum << " выполняет свою работу;" << std::endl;
+	std::cout << "Поток № " << lpParam << " выполняет свою работу;" << std::endl;
 	LeaveCriticalSection(&cs);
 	ExitThread(0);
 }
@@ -25,7 +25,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	for (int i = 0; i < N; i++) 
 	{
-		handles[i] = CreateThread(NULL, 0, ThreadProc, (LPVOID)(intptr_t)(i + 1), 0, NULL);
+		handles[i] = CreateThread(NULL, 0, ThreadProc, &i, 0, NULL);
 	}
 
 	WaitForMultipleObjects(N, handles, true, INFINITE);
